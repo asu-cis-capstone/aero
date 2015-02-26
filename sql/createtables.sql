5b9121a9c079cdebbeca5903c7261dc4fd205aba
Ethan Selin <ethanselin@gmail.com>
2015-02-26 00:41:25 -0700

    added images and faa tables

--- sql/createtables.sql
+++ sql/createtables.sql
@@ -13,3 +13,21 @@ cText varchar(250),
 qID int(4),
 FOREIGN KEY (qID) REFERENCES test_questions(qID)
 );
+
+CREATE TABLE images
+(
+	img_id int not null auto_increment,
+    img_name varchar(50),
+    img blob,
+    PRIMARY KEY(img_id)
+);
+
+CREATE TABLE faa
+(
+	ls_code char(6) not null,
+    descrip varchar(100) not null,
+    subj varchar(30) not null,
+    topic varchar(30) not null,
+    subtop varchar(30)
+    
+);
