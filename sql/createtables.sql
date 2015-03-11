a1032614fe85cbf639eebfe75d85aa4f92d1ae97
Ethan Selin <ethanselin@gmail.com>
2015-03-11 13:16:30 -0700

    Update createtables.sql
    changed structure of tables to tie them together. Still need to create Aircraft table but have not received that information

--- sql/createtables.sql
+++ sql/createtables.sql
@@ -1,18 +1,33 @@
-CREATE TABLE test_questions
-(
-qID int(4),
-qText varchar(250)
+CREATE TABLE test_questions(
+
+	qID int(4) not null,
+	qText varchar(250) not null,
+	ls_code char(6) not null,
+	PRIMARY KEY(qID),
+	FOREIGN KEY(ls_code) REFERENCES faa(ls_code)
+	
 );
 
-CREATE TABLE test_answers
-(
-answer char(1),
-aText varchar(250),
-bText varchar(250),
-cText varchar(250),
-qID int(4),
-FOREIGN KEY (qID) REFERENCES test_questions(qID)
+CREATE TABLE test_answers(
+
+	answer char(1) not null,
+	aText varchar(250) not null,
+	bText varchar (250) not null,
+	cText varchar (250) not null,
+	qID int(4) not null,
+	FOREIGN KEY (qID) REFERENCES test_questions(qID)
+	
 );
+
+CREATE TABLE faa(
+	ls_code char(6) not null,
+	descrip varchar(100) not null,
+	subj varchar(30) not null,
+	topic varchar(30) not null,
+	subtop varchar(30)
+
+);
+
 
 CREATE TABLE images
 (
@@ -22,12 +37,3 @@ CREATE TABLE images
     PRIMARY KEY(img_id)
 );
 
-CREATE TABLE faa
-(
-	ls_code char(6) not null,
-    descrip varchar(100) not null,
-    subj varchar(30) not null,
-    topic varchar(30) not null,
-    subtop varchar(30)
-    
-);
