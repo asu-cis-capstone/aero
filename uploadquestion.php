7452631e7f2047fe16fe16f6d6bcf6c58b647afe
Jose Arballo <josefarballo@gmail.com>
2015-03-24 11:36:12 -0700

    edited upload question form with correct attributes

--- uploadquestion.php
+++ uploadquestion.php
@@ -41,25 +41,29 @@ Edit Question Page for Aeroapps Technology
 		<center>
 		<p id="title">Add a Question</p>
 		
-		<form id="questionform" action="demo_form.asp">
+		<form id="questionform" action="questionprocess.php" method="post">
 			<h2>Enter Question Information Below</h2>
-			<label>Test</label> <input type="text"><br />
-			<label>Question ID</label> <input type="text"><br /> 
-  			<label>Question Text:</label>
-  			<textarea rows="5" cols="30"></textarea><br />
-  			<label>Answer Choice A</label> <input type="text"><br />
-  			<label>Answer Choice B</label> <input type="text"><br />
-  			<label>Answer Choice C</label> <input type="text"><br />
+			<label for="test">Test</label> <input type="text" name="test"><br />
+			<label for="qID">Question ID</label> <input type="text" name="qID"><br /> 
+  			<label for="qText">Question Text:</label>
+  			<textarea name="qText" rows="5" cols="30"></textarea><br />
+  			<label for="qImg1">Question Image 1:</label> <input type="file" name="qImg1">
+  			<label for="qImg2">Question Image 2:</label> <input type="file" name="qImg2">
+  			<label for="qImg3">Question Image 3:</label> <input type="file" name="qImg3">
+  			<label for="qImg4">Question Image 4:</label> <input type="file" name="qImg4">
+  			<label for="qImg5">Question Image 5:</label> <input type="file" name="qImg5">
+  			<label for="aText">Answer Choice A</label> <input type="text" name="aText"><br />
+  			<label for="bText">Answer Choice B</label> <input type="text" name="bText"><br />
+  			<label for="cText">Answer Choice C</label> <input type="text" name="cText"><br />
   			<label>Correct Answer:</label>
   			<SELECT name="Single-line ListBox example">
 			<OPTION selected value="A">A</OPTION>
 			<OPTION value="B">B</OPTION>
 			<OPTION value="C">C</OPTION>
 			</SELECT><br />
-			<label>Learning Statement Code</label> <input type="text"><br />
-			<label>Reference Name</label> <input type="text"><br />
-			<label>Upload an Image:</label> <input type="file" name="img">
-  			<br />
+			<label for="ls_code">LS Code</label> <input type="text" name="ls_code"><br />
+			<label for="refName">Reference Name</label> <input type="text" name="refName"><br />
+			<label for="refPincite">Reference Pincite</label> <input type="text" name="refPincite"><br />
   			<label>Select Existing Image:</label>
   			<SELECT name="Single-line ListBox example">
 			<OPTION selected value="1">1</OPTION>
